bc1f7bc9ecc9982147dab18920ef8125df50225d
Martin Morris <martinryanmorris@gmail.com>
2024-02-04 14:45:27 +1000

    able to render a basic canvas and the etch works

--- functions.js
+++ functions.js
@@ -1,27 +1,57 @@
 /* Functions for Etch a sketch */
 
-
 // create a canvas of size by size
 function createCanvas(size) {
-  // TODO select parent canvas
-  let canvas = document.createElement('div');
+  // select parent canvas
+  let canvas = document.querySelector("#canvas");
+  canvas.innerHTML = "";
 
-  
-  
-  for(let i = 0; i < size; i++) {
-    let row = document.createElement('div');
-    row.setAttribute('class', 'row');
-    for(let r = 0; r < size; r++) {
-      let cell = document.createElement('div');
-      cell.setAttribute('class', 'cell');
+  for (let i = 0; i < size; i++) {
+    let row = document.createElement("div");
+    row.setAttribute("class", "row");
+    for (let r = 0; r < size; r++) {
+      let cell = document.createElement("div");
+      cell.setAttribute("class", "cell");
+      cell.addEventListener("mouseenter", function () {
+        darkenCell(cell);
+      });
       row.appendChild(cell);
     }
     canvas.appendChild(row);
   }
-
-  // TODO change to display
-  console.log(canvas);
 }
 
+// TODO add a param to fill in with color X or generate a random 1
+function darkenCell(element) {
+  //get the current colour of the cell
+  let currentColor = element.getAttribute("style");
 
-createCanvas(16);
+  let backgroundColor = "";
+
+  if (currentColor == null) {
+    backgroundColor =
+      "background-color: rgba(" + generateRandomColorValue() + "0.2);";
+  } else {
+    // find the start and end of the opacity then get the opacity
+    let pos1 = currentColor.lastIndexOf(", ") + 2;
+    let pos2 = currentColor.lastIndexOf(")");
+    let length = pos2 - pos1;
+    let opacity = currentColor.substr(pos1, length);
+    //if opacity is not 1 make it darker
+    if (opacity != "1") {
+      opacity = Number(opacity) + 0.2;
+    }
+    backgroundColor = currentColor.substr(0, pos1) + opacity + ");";
+  }
+
+  element.setAttribute("style", backgroundColor);
+}
+
+function generateRandomColorValue() {
+  let r = Math.floor(Math.random() * 255);
+  let g = Math.floor(Math.random() * 255);
+  let b = Math.floor(Math.random() * 255);
+  return r + ", " + g + ", " + b + ", ";
+}
+
+createCanvas(150);
